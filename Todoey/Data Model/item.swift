5081acb36f951e8b8429944e63b693291f4e44ef
Andrey <chekmenev.av@gmail.com>
2019-04-17 15:36:09 +0300

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,15 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Andrey Chekmenev on 17/04/2019.
+//  Copyright © 2019 Andrey Chekmenev. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
